886f520c07befcd8084ac4ba6e066a99e553f74a
Alex Sutton <mandarinfish27@aol.com>
2023-09-07 20:47:52 -0600

    lab progress

--- making-decisions-2/makingDecisions2Lab.js
+++ making-decisions-2/makingDecisions2Lab.js
@@ -5,9 +5,8 @@ var arr = [10,20,30,40,50,60]
 /*
   Create a variable named 'firstItem' and set it equal to the first value of the 'arr' array.
 */
-
-let index = 0
-console.log(arr[index])
+let firstItem = arr[0]
+console.log(firstItem)
 
 
 ////////// PROBLEM 2 //////////
@@ -29,8 +28,9 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
   Loop through the 'family' array and console.log every item.
 */
 
-//Code Here
-
+for (let i = 0; i < family.length; i++) {
+    console.log(family[i])
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -44,8 +44,12 @@ let evensArr = []
   Use a for-loop to iterate through each number in the 'nums' array, if the number is even, append it to the 'evensArr'.
 */
 
-//Code Here
-
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+        evensArr.push(nums[i])
+    }
+}
+console.log(evensArr)
 
 
 ////////// PROBLEM 5 //////////
@@ -57,8 +61,28 @@ var score = 74
 /*
   Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
 */
-
-//Code Here
+// let letterGrade = "C"
+if (score >= 60 && score <= 69) {
+   let letterGrade = "D"
+    console.log(letterGrade)
+}
+    else if (score >= 70 && score <= 79) {
+        let letterGrade = "C"
+    console.log(letterGrade)
+    }
+    else if (score >= 80 && score <= 89) {
+        let letterGrade = "B"
+    console.log(letterGrade)
+    }
+    else if (score >= 90) {
+        let letterGrade = "A"
+    console.log(letterGrade)
+    }
+    else {
+        let letterGrade = "F"
+    console.log(letterGrade)
+    }
+// console.log(letterGrade)
 
 
 ////////// Intermediate Problems //////////
@@ -73,15 +97,15 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
   Create a variable named 'someNum' and set it equal to the fifth value of the 'myFavoriteNumbers' array.
 */
 
-//Code Here
-
+var someNum = myFavoriteNumbers[4]
+console.log(someNum)
 
 ////////// PROBLEM 7 //////////
 
 // Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, but there are not 7 elements in the array? Let's write some code to check for that. 
 // Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
-//Code Here
+if (myFavoriteNumbers.length > 7)
 
 
 ////////// PROBLEM 8 //////////
